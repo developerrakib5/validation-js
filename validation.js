830d61c0f4443bb7c5fc7e22cd5714ed55c08d75
devlrakib <devrakib5@gmail.com>
2024-05-03 14:24:29 +0600

    first commit

---
+++ validation.js
@@ -0,0 +1,18 @@
+const number = '23';
+// String,Number,Boolean
+if (typeof number == 'number'){
+   console.log('Value Is A Number');
+}
+else{
+    console.log('Not A Number');
+}
+
+
+const numbers = [13,12,78,90];
+const student = {
+    name: 'Sakib Al Hasan',
+    Job : 'Khawa Dawa Kora'
+}
+//console.log(typeof(numbers));
+console.log(Array.isArray(numbers));
+console.log(typeof(student));
